fe2bc99ddd02c001193f0e8317cc1a4850acfeca
Enrico <enricosellanoradev@gmail.com>
2020-11-23 22:24:59 +0100

    add
    add

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import Clock from './Clock';
 
+const clocks = [
+  {
+    timezone : 0,
+    country : 'Italy'
+  },
+  {
+    timezone : 2,
+    country : 'Russia'
+  },
+  {
+    timezone :  -6,
+    country : 'Cuba'
+  },
+]
 
 class App extends Component {
+
+  getClocs(){
+    return clocks.map((clock) => {return <Clock key = {clock.country} timezone={clock.timezone} country={clock.country}/>});
+  }
+
   render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Clock in React</h1>
-        <Clock timezone='0' country='Italy'/>
-        <Clock timezone='2' country='Russia'/>
-        <Clock timezone='-6' country='Cuba'/>
+        <ul>{this.getClocs()}</ul>
       </header>
     </div>
   );
--- src/Clock.js
+++ src/Clock.js
@@ -1,11 +1,15 @@
 import React, {Component}  from 'react';
 
+
+
 class Clock extends Component {
 
   constructor(props){
     super(props);
+    //this.toggleWatch = this.toggleWatch.bind(this)
     this.state = {
-      date : new Date()
+      date : new Date(),
+      stopped : false
     };
   }
   render(){
@@ -13,19 +17,35 @@ class Clock extends Component {
     const time = this.state.date.getTime() + this.props.timezone*secondInaHour* 1000;
     const date = new Date(time);
 
-    return <h2>In {this.props.country} it'is {date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}</h2>
+    return <li>In {this.props.country} it'is <span className='clock'>{date.toLocaleDateString() + ' ' + date.toLocaleTimeString()} </span><button onClick={this.toggleWatch}>{this.state.stopped ? 'START' : 'STOP'}</button></li>
+    
   }
+
+  toggleWatch = (click) => {
+
+    this.setState((state,props) => {
+      state.stopped ? this.startWatch() : clearInterval(this.interval);
+      return {stopped: ! state.stopped};
+    })
+
+  };
+
   tick = () => {
     this.setState({
       date : new Date()
     })
-};
-  componentDidMount(){
+  };
+
+  startWatch(){
     this.interval = setInterval(this.tick, 1000)
-  }
+  };
+
+  componentDidMount(){
+    this.startWatch ()
+  };
   componentWillUnmount(){
     clearInterval(this.interval)
-  }
+  };
 }
 
 
